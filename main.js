1336876a18565d01703dda47fbe83611a26e72ba
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2022-02-05 20:46:04 +0300

    First Js File

---
+++ main.js
@@ -0,0 +1,26 @@
+let cards = document.querySelectorAll(".ghosts .box");
+let stars = document.querySelectorAll(".far.fa-star");
+let shadyStars = document.querySelectorAll(".filled.fas.fa-star.shady")
+let shadyStarsEmpty = document.querySelectorAll(".far.fa-star.shady")
+
+function allStars() {
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].className = "filled fas fa-star";
+    }
+}
+function choxStars() {
+    for (let i = 0; i < shadyStars.length; i++) {
+        shadyStars[i].className = "far fa-star";
+    }
+}
+function choxStarsEmpty() {
+    for (let i = 0; i < shadyStarsEmpty.length; i++) {
+        shadyStarsEmpty[i].className = "far fa-star shady";
+    }
+}
+
+for (i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", allStars);
+    cards[i].addEventListener("click", choxStars);
+    cards[i].addEventListener("click", choxStarsEmpty);
+}
